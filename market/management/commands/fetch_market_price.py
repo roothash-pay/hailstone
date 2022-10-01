16d2cf72503c5f1d0430380b1ba9a0c9a6d80ed5
guoshijiang <guoshijiang2012@163.com>
2022-10-02 00:06:40 +0800

    fix

--- market/management/commands/fetch_market_price.py
+++ market/management/commands/fetch_market_price.py
@@ -6,7 +6,7 @@ from django.core.management.base import BaseCommand
 from common.helpers import d0, dec, sleep
 from services.market_client import MarketClient
 from services.savour_rpc import common_pb2
-from market.models import StablePrice, Asset, MarketPrice, Symbol
+from market.models import StablePrice, Asset, MarketPrice, Symbol, Exchange
 
 
 class Command(BaseCommand):
@@ -19,17 +19,30 @@ class Command(BaseCommand):
         if len(symbol_result.symbol_prices) == 0:
             logging.warning(symbol_result)
             return
-
-        # symbol_id_list = []
+        print(symbol_result)
+        symbol_list = []
+        exchange_list = []
         asset_name_list = []
+
         for item in symbol_result.symbol_prices:
-            # symbol_id_list.append(item.id)
             asset_name_list.append(item.base)
             asset_name_list.append(item.quote)
+            exchange_list.append(item.exchange)
+            symbol_list.append(item.symbol)
 
         price_list = []
 
         asset_dict = {}
+        symbol_dict = {}
+        exchange_dict = {}
+        symbols = Symbol.objects.filter(name__in=symbol_list)
+        for symbol in symbols:
+            symbol_dict[symbol.name] = symbol
+
+        exchanges = Exchange.objects.filter(name__in=exchange_list)
+        for exchange in exchanges:
+            exchange_dict[exchange.name] = exchange
+
         asset_list = Asset.objects.filter(name__in=asset_name_list)
         for asset in asset_list:
             asset_dict[asset.name] = asset
@@ -50,8 +63,8 @@ class Command(BaseCommand):
                     buy_price=item.buy_price,
                     sell_price=item.sell_price,
                     margin=item.margin,
-                    # symbol_id=int(item.id),
-                    # exchange= NONE,
+                    symbol=symbol_dict[item.symbol],
+                    exchange=exchange_dict[item.exchange],
                     qoute_asset=quote_asset,
                     base_asset=base_asset,
                 ))
