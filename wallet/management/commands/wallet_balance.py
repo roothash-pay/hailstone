a03d9e0cf357e48b1b2b7f35737ab380ce6abc1a
guoshijiang <guoshijiang2012@163.com>
2023-03-18 21:49:49 +0800

    add wallet balance stat

--- wallet/management/commands/wallet_balance.py
+++ wallet/management/commands/wallet_balance.py
@@ -1,20 +1,20 @@
 #encoding=utf-8
 
 from django.core.management.base import BaseCommand
-from wallet.models import WalletAsset, AddressAsset
+from wallet.models import Wallet, AddressAsset
 from common.helpers import d0
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        wallet_asset_list = WalletAsset.objects.all()
-        for wallet_asset in wallet_asset_list:
-            address_asset_list = AddressAsset.objects.filter(wallet=wallet_asset.wallet, asset=wallet_asset.asset)
+        wallet_list = Wallet.objects.all()
+        for wallet in wallet_list:
             wallet_asset_usd = d0
             wallet_asset_cny = d0
+            address_asset_list = AddressAsset.objects.filter(wallet=wallet)
             for address_asset in address_asset_list:
                 wallet_asset_usd += address_asset.asset_usd
                 wallet_asset_cny += address_asset.asset_cny
-            wallet_asset.asset_usd = wallet_asset_usd
-            wallet_asset.asset_cny = wallet_asset_cny
-            wallet_asset.save()
+            wallet.asset_usd = wallet_asset_usd
+            wallet.asset_cny = wallet_asset_cny
+            wallet.save()
